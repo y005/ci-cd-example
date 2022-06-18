1380a6feded8de9920d1270f667fbca4504bca74
MOON <y005@naver.com>
2022-06-18 21:27:11 +0900

    update configure

---
+++ src/main/java/com/example/demo/MyRestController.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestController {
+  @GetMapping("/api/v1/test")
+  public String get() {
+    return "hello world";
+  }
+}
