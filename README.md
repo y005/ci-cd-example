8357578d141ada404c23e027bf55740e8c84fbe1
MOON <y005@naver.com>
2022-06-19 12:55:25 +0900

    no message

--- README.md
+++ README.md
@@ -147,17 +147,9 @@ cd $REPOSITORY
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | head -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-#현재 실행되고 있는 어플리케이션 pid 확인
-CURRENT_PID = $(sudo netstat -tnlp | grep 8080 | gawk '{ print $7 }' | grep -o '[0-9]*')
+#현재 실행되고 있는 포트 8080 어플리케이션 종료
+sudo kill -15 `sudo netstat -tnlp|grep 8080 |gawk '{ print $7 }'|grep -o '[0-9]*'`
 
-if [ -z $CURRENT_PID ]
-then
-  echo '프로그램 발견 안됨'
-else
-  echo '프로그램 발견됨'
-  kill -15 $CURRENT_PID
-  sleep 5
-fi
 
 #배포된 파일을 백그라운드 모드로 실행하면서 로그아웃 후에도 프로세스가 죽지 않고 진행되고
 #실행 파일에 의해 발생되는 출력을 화면에 보이지 않게 하는 명령어
--- deploy.sh
+++ deploy.sh
@@ -7,21 +7,8 @@ JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | head -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 #현재 실행되고 있는 어플리케이션 pid 확인
-CURRENT_PID = $(sudo netstat -tnlp | grep 8080 | gawk '{ print $7 }' | grep -o '[0-9]*')
-sudo kill -9 `sudo netstat -tnlp|grep 8080 |gawk '{ print $7 }'|grep -o '[0-9]*'`
-
-if [ -z $CURRENT_PID ]
-then
-  echo "종료해야 하는 자바 프로그램 없음"
-else
-  echo "종료 시켜야 되는 자바 프로그램 발견"
-  kill -15 $CURRENT_PID
-  sleep 5
-fi
+sudo kill -15 `sudo netstat -tnlp|grep 8080 |gawk '{ print $7 }'|grep -o '[0-9]*'`
 
 #배포된 파일을 백그라운드 모드로 실행하면서 로그아웃 후에도 프로세스가 죽지 않고 진행되고
 #실행 파일에 의해 발생되는 출력을 화면에 보이지 않게 하는 명령어
 nohup java -jar $JAR_PATH 2>&1 &
-#echo "$CURRENT_PID" > test.txt
-
-#nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
