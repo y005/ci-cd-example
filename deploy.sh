c00a77e030536238acdb6be1705f37744a1eebf6
MOON <y005@naver.com>
2022-06-19 13:09:29 +0900

    no message

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 sudo kill -15 `sudo netstat -tnlp|grep 8080 |gawk '{ print $7 }'|grep -o '[0-9]*'`
 
-nohup java -jar $JAR_PATH dev/null 2> /dev/null < /dev/null &
+nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
