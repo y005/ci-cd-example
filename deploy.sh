45bc47a61304cf4e3c221701d95b79371202340b
MOON <y005@naver.com>
2022-06-19 11:07:14 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,9 +11,9 @@ CURRENT_PID=$(pgrep -fl action | grep java | awk '{print $1}')
 
 if [ -z $CURRENT_PID ]
 then
-  echo '종료해야 하는 자바 프로그램 없음'
+  echo "종료해야 하는 자바 프로그램 없음"
 else
-  echo '종료 시켜야 되는 자바 프로그램 발견'
+  echo "종료 시켜야 되는 자바 프로그램 발견"
   kill -15 $CURRENT_PID
   sleep 5
 fi
@@ -21,6 +21,6 @@ fi
 #배포된 파일을 백그라운드 모드로 실행하면서 로그아웃 후에도 프로세스가 죽지 않고 진행되고
 #실행 파일에 의해 발생되는 출력을 화면에 보이지 않게 하는 명령어
 nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
-echo '$JAR_PATH' > file1.txt
-echo '$JAR_NAME' > file2.txt
+echo "$JAR_PATH" > file1.txt
+echo "$JAR_NAME" > file2.txt
 #nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
