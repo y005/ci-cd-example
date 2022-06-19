6adaae7c43c4272da4e0a0148ad5f949cb443b44
MOON <y005@naver.com>
2022-06-19 13:19:35 +0900

    no message

--- src/main/java/com/example/demo/MyRestController.java
+++ src/main/java/com/example/demo/MyRestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyRestController {
   @GetMapping("/api/v1/test")
   public String start() {
-    return "드디어 실습 완성했다 ㅜㅜ";
+    return "Hello World";
   }
 }
