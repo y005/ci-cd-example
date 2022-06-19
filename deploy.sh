4f94007c09c174f27de286e78c25c3d1443e2f24
MOON <y005@naver.com>
2022-06-19 12:51:41 +0900

    no message

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 #현재 실행되고 있는 어플리케이션 pid 확인
 CURRENT_PID = $(sudo netstat -tnlp | grep 8080 | gawk '{ print $7 }' | grep -o '[0-9]*')
+sudo kill -9 `sudo netstat -tnlp|grep 8080 |gawk '{ print $7 }'|grep -o '[0-9]*'`
 
 if [ -z $CURRENT_PID ]
 then
@@ -21,6 +22,6 @@ fi
 #배포된 파일을 백그라운드 모드로 실행하면서 로그아웃 후에도 프로세스가 죽지 않고 진행되고
 #실행 파일에 의해 발생되는 출력을 화면에 보이지 않게 하는 명령어
 nohup java -jar $JAR_PATH 2>&1 &
-echo "$CURRENT_PID" > test.txt
-sudo netstat -tnlp | grep 8080 | gawk '{ print $7 }' | grep -o '[0-9]*' > test1.txt
+#echo "$CURRENT_PID" > test.txt
+
 #nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
