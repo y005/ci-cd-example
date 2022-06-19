c0c928d983b91b3fdff8cd9ab0f56ffda7617604
MOON <y005@naver.com>
2022-06-19 10:41:15 +0900

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,7 +20,8 @@ fi
 
 #배포된 파일을 백그라운드 모드로 실행하면서 로그아웃 후에도 프로세스가 죽지 않고 진행되고
 #실행 파일에 의해 발생되는 출력을 화면에 보이지 않게 하는 명령어
-nohup java -jar $JAR_PATH &
+sudo nohup java -jar $JAR_PATH &
 
-mkdir moon
+mkdir $JAR_PATH
+mkdir $JAR_NAME
 #nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
